b587542460bd904927dfab40440867a68d5f7485
unknown <lungrbackup@protonmail.com>
2021-09-17 13:24:06 -0400

    cool stiinky update

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,17 @@
 var pmodal;
 var lmodal;
+var dmodal;
 var span;
 var spanl;
+var spand;
 document.onload = open();
 function open(){
     span = document.getElementsByClassName("close")[0];
     spanl = document.getElementById("close");
+    spand = document.getElementById("dclose");
     pmodal = document.getElementById("purchasemodal");
     lmodal = document.getElementById("loginmodal");
+    dmodal = document.getElementById("debugmodal");
 }
 function pmodelOpen(){
     pmodal.style.display = "block";
@@ -15,11 +19,17 @@ function pmodelOpen(){
 function lmodelOpen(){
     lmodal.style.display = "block";
 }
+function dmodelopen(){
+    dmodal.style.display = "block";
+}
 span.onclick = function() {
     pmodal.style.display = "none";
 }
 spanl.onclick = function() {
     lmodal.style.display = "none";
+}
+spand.onclick = function() {
+    dmodal.style.display = "none";
 }
 window.onclick = function(event) {
     if (event.target == pmodal) {
